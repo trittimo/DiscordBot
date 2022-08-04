5c569b46c3b8bc758d9105a5c0baa9c02ab599d5
Michael Trittin <michaeltrittin@gmail.com>
2022-08-04 00:07:02 -0400

    Apparently you must have a port 80 active/listening for the app service to continue running

--- src/main/java/com/trittimo/discordbot/App.java
+++ src/main/java/com/trittimo/discordbot/App.java
@@ -1,5 +1,8 @@
 package com.trittimo.discordbot;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.Properties;
 
 import com.azure.core.exception.ResourceNotFoundException;
@@ -12,6 +15,9 @@ import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 
 public class App {
     public static final String KEY_VAULT_NAME = System.getenv("KEY_VAULT_NAME");
@@ -28,12 +34,44 @@ public class App {
             KeyVaultSecret discordTokenSecret = secretClient.getSecret("discord-token");
             Properties properties = new Properties();
             properties.setProperty("DISCORD_TOKEN", discordTokenSecret.getValue());
+            App.startWebListener();
             App.run(properties);
         } catch (ResourceNotFoundException e) {
             e.printStackTrace();
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void startWebListener() {
+        Runnable webListener = new Runnable() {
+            public void run() {
+                try {
+                    HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+                    server.createContext("/", new HttpHandler() {
+                        public void handle(HttpExchange t) {
+                            System.out.println("Somebody attempted to access port 80 with a GET request");
+                            String response = "This keeps the app running!";
+                            try {
+                                t.sendResponseHeaders(200, response.length());
+                                OutputStream s = t.getResponseBody();
+                                s.write(response.getBytes());
+                                s.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            
+                        }
+                    });
+                    server.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread thread = new Thread(webListener);
+        thread.start();
     }
 
     public static void run(Properties properties) {
